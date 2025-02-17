4e9b061c92c41e2c1cf275489bb9c7d8b835b234
Daniel <Daniel@DESKTOP-EICR6RV>
2025-02-17 12:00:45 +0100

    implemented record class and fisrt method in db manager

--- CodingTracker/CodingTracker/CodingSession.cs
+++ CodingTracker/CodingTracker/CodingSession.cs
@@ -6,5 +6,32 @@ namespace CodingTracker
     /// </summary>
     internal class CodingSession
     {
+        public int ID { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public TimeSpan Duration { get; set; }
+        public CodingSession()
+        {
+            //setup default values
+            ID = 0;
+            Start = DateTime.MinValue;
+            End = DateTime.MinValue;
+            Duration = TimeSpan.Zero;
+        }
+        public CodingSession(int id, DateTime start, DateTime end)
+        {
+            ID = id;
+            Start = start;
+            End = end;
+            Duration = start - end;
+        }
+        public CodingSession(int id, DateTime end, DateTime start, TimeSpan duration)
+        {
+            ID = id;
+            Start = start;
+            End = end;
+            Duration = duration;
+        }
+
     }
 }
--- CodingTracker/CodingTracker/DatabaseManager.cs
+++ CodingTracker/CodingTracker/DatabaseManager.cs
@@ -1,9 +1,30 @@
-﻿namespace CodingTracker
+﻿using Dapper;
+using System.Data.SQLite;
+
+namespace CodingTracker
 {
     /// <summary>
     /// Managing reading and writing to the Coding Tracker database
     /// </summary>
     internal class DatabaseManager
     {
+        public string ConnectionString { get; init; }
+        public string DatabaseName { get; init; }
+        public DatabaseManager(string connectionString, string databaseName)
+        {
+            ConnectionString = connectionString;    
+            DatabaseName = databaseName;
+        }
+        private void CreateDatabase()
+        {
+            string sql = $"CREATE DABASE {DatabaseName};";
+
+            using (var connection = new SQLiteConnection(ConnectionString)) 
+            { 
+                connection.Execute(sql);
+            }
+        }
+        
+
     }
 }
